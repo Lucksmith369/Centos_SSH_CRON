8b9f0a532a3c12d010dae3e5c33ec8ef15b6c410
Dennis Acker <Dennis.Acker@lucksmith.de>
2020-07-14 21:14:19 +0200

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
+cp checkSSHPort /usr/bin/checkSSHPort
+cp checkService /usr/bin/checkService
+chmod +x /usr/bin/checkService
+chmod +x /usr/bin/checkSSHPort
 crontab -l | { cat; echo "* * * * * /bin/bash -l /usr/bin/checkSSHPort"; } | crontab -
 crontab -l | { cat; echo "* * * * * /bin/bash -l /usr/bin/checkService sshd"; } | crontab -
